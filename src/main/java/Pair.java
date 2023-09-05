b919b3f6ab7cc01ddf3559f6ef84c7e2ccba120f
Yashaswi Pratick <yashaswipratick@Yashaswis-MacBook-Pro.local>
2023-09-05 16:49:04 +0530

    length of the longest substring

---
+++ src/main/java/Pair.java
@@ -0,0 +1,56 @@
+import java.util.Objects;
+
+public class Pair {
+    private int key;
+    private int value;
+
+    public Pair(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Pair() {
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return getKey() == pair.getKey();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getKey());
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return getKey() == pair.getKey() && getValue() == pair.getValue();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getKey(), getValue());
+    }
+}
---
+++ src/main/java/UshurQuestionII.java
@@ -0,0 +1,75 @@
+package src.main.java;
+
+import java.util.HashMap;
+
+public class UshurQuestionII {
+
+    /*
+    Given a string, find the length of the longest substring in it with no more than K distinct
+    characters.
+    You can assume that K is less than or equal to the length of the given string.
+
+    Example 1:
+
+    Input: String="araaci", K=2
+    Output: 4
+    Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+    Example 2:
+
+    Input: String="araaci", K=1
+    Output: 2
+    Explanation: The longest substring with no more than '1' distinct characters is "aa".
+
+    Example 3:
+
+    Input: String="cbbebi", K=3
+    Output: 5
+    Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
+     */
+    public static int getLongestSubstringLength(String s, int K) {
+        int count = 0;
+        int i = 0;
+        int maxCount = 0;
+        int j = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (map.size() < K) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                count++;
+            } else if (map.containsKey(c)) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                count++;
+            } else {
+                count--;
+                int KCount = K;
+                while (map.size() == KCount) {
+                    Integer charCount = map.get(s.charAt(j));
+                    charCount -= 1;
+                    if (charCount == 0) {
+                        map.remove(s.charAt(j));
+                    } else {
+                        map.put(s.charAt(j), charCount);
+                    }
+                    j++;
+                }
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                count++;
+            }
+            maxCount = Math.max(count, maxCount);
+            i++;
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        String s = "araaci";
+        System.out.println(getLongestSubstringLength(s, 2));
+
+        String s1 = "araaci";
+        System.out.println(getLongestSubstringLength(s1, 1));
+
+        String s2 = "cbbebi";
+        System.out.println(getLongestSubstringLength(s2, 3));
+    }
+}
